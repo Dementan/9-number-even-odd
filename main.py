5c16462a80e9863a8fdf193aaba7b0457afb42f8
Dementan <masav7986@gmail.com>
2023-02-17 18:26:20 +0300

    program to determine if a number is even or odd

---
+++ main.py
@@ -0,0 +1,7 @@
+#program to determine if a number is even or odd
+
+number = int(input("Enter any number: "))
+if number % 2 == 0:  #divided by two without a remainder
+    print("It is an even number")
+else:
+    print("It is an odd number")
